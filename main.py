37a42d805f9ef4ea26d64285317e83d66565a1d0
Shah rahman <toshah.rahman@gmail.com>
2022-05-30 17:50:15 +0300

    fastapi-webapp fun 6

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ app = fastapi.FastAPI()
 fastapi_chameleon.global_init("templates")
 
 
-@app.get('/')
-@template(template_file='index.html')
+@app.get("/")
+@template(template_file="index.html")
 def index():
-    return {'user_name': 'Shah Rahman'}
+    return {"user_name": "Shah Rahman"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app)
