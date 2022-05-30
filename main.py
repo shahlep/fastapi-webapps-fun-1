487e91a5f984ca96649113d49d7e9d7c15797fd3
Shah rahman <toshah.rahman@gmail.com>
2022-05-30 17:44:57 +0300

    fastapi-webapp fun 4

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import fastapi
 import uvicorn
+import fastapi_chameleon
+from fastapi_chameleon import template
 
 app = fastapi.FastAPI()
 
+fastapi_chameleon.global_init("templates")
+
 
 @app.get('/')
+@template(template_file='index.html')
 def index():
-    content = """
-    
-    <h1>Web app fun experiment </h1>
-    <div>html template </div>
-    """
-    resp = fastapi.responses.HTMLResponse(content)
-    return resp
+    return {'user_name': 'Shah Rahman'}
 
 
 if __name__ == '__main__':
