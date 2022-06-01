4bb8306cedadaec598164ea747635c81f70ef0b3
Shah rahman <toshah.rahman@gmail.com>
2022-06-01 16:42:58 +0300

    fastapi-webapp fun 23

--- views/packages.py
+++ views/packages.py
@@ -1,8 +1,13 @@
 import fastapi
+import fastapi_chameleon
+from fastapi_chameleon import template
 
 router = fastapi.APIRouter()
 
+fastapi_chameleon.global_init("templates")
+
 
 @router.get("/")
+@template()
 def index():
-    pass
+    return {}
